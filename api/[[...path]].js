3a7061ab50a12a0a25fbfe0b82ef7319c85999b7
saadaccellionx-2 <saadaccellionx@gmail.com>
2026-02-16 23:58:03 +0000

    nmmcnv,mcxnv

--- api/[[...path]].js
+++
@@ -1,6 +0,0 @@
-/**
- * Vercel serverless entry: forwards all /api/* requests to the Express app.
- * Required because Vercel does not run a long-lived Node server (e.g. bin/www).
- */
-const app = require("../app");
-module.exports = app;
